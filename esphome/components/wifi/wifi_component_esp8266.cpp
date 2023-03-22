0285d057556e048795c5051c7679c42fcc8783cf
eavyon <84970882+eavyon@users.noreply.github.com>
2023-03-22 20:12:31 +0100

    Update wifi_component_esp8266.cpp

--- esphome/components/wifi/wifi_component_esp8266.cpp
+++ esphome/components/wifi/wifi_component_esp8266.cpp
@@ -22,12 +22,14 @@ extern "C" {
 #endif
 #if USE_ARDUINO_VERSION_CODE >= VERSION_CODE(3, 0, 0) && USE_ARDUINO_VERSION_CODE < VERSION_CODE(3, 1, 0)
 #include "LwipDhcpServer.h"
-#define wifi_softap_set_dhcps_lease(lease) dhcpSoftAP.set_dhcps_lease(lease)
-#define wifi_softap_set_dhcps_lease_time(time) dhcpSoftAP.set_dhcps_lease_time(time)
-#define wifi_softap_set_dhcps_offer_option(offer, mode) dhcpSoftAP.set_dhcps_offer_option(offer, mode)
 #endif
 #if USE_ARDUINO_VERSION_CODE >= VERSION_CODE(3, 1, 0)
 auto &dhcpServer = WiFi.softAPDhcpServer();
+#endif
+#if USE_ARDUINO_VERSION_CODE >= VERSION_CODE(3, 0, 0)
+#define wifi_softap_set_dhcps_lease(lease) dhcpSoftAP.set_dhcps_lease(lease)
+#define wifi_softap_set_dhcps_lease_time(time) dhcpSoftAP.set_dhcps_lease_time(time)
+#define wifi_softap_set_dhcps_offer_option(offer, mode) dhcpSoftAP.set_dhcps_offer_option(offer, mode)
 #endif
 }
 
